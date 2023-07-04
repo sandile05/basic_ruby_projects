4714900ff2a31c284f1c08cbb032c699a1749108
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-07-04 14:57:58 +0200

    check if word in dictionary is in string

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,9 +1,22 @@
+#dictionary array
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 #convert string to downcase array
 #determine if each element is included in the dictionary and if it has a  hash key
 #if hash key has no value add a value => 1, if it does count the value as 1
-def substring(string, dictionary)
+def substrings(string, dictionary)
   hash = {}
   string_array =  string.split
+  string_array.each do |i|
+    dictionary.each do |s|
+      if i.include?(s)
+        if hash.has_key?(s)
+          hash[s] += 1
+        else hash[s] = 1
+        end
+      end
+    end
+  end
 
 
 end

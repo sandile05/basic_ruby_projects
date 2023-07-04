0eaa3d48dd6be7f275acef6fe487217bd423d106
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-07-04 19:47:32 +0200

    bubble sort

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,23 @@
+def bubble_sort(array)
+  n = array.length
+
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+
+end
+
+a = [4,3,78,2,0,2]
+
+p bubble_sort(a)

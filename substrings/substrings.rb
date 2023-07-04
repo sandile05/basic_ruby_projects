6fb11753622db982b22ecf7438640f401ff0686c
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-07-04 14:45:55 +0200

    emty hash,  string to downcase array

---
+++ substrings/substrings.rb
@@ -0,0 +1,9 @@
+#convert string to downcase array
+#determine if each element is included in the dictionary and if it has a  hash key
+#if hash key has no value add a value => 1, if it does count the value as 1
+def substring(string, dictionary)
+  hash = {}
+  string_array =  string.split
+
+
+end
